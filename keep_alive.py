fbe91af27176d222be618a0223dcf50756248e74
hirshad494 <142832851+hirshad494@users.noreply.github.com>
2023-08-21 14:19:29 -0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,24 @@
+# Import the Flask framework to create a simple web server
 from flask import Flask
+# Import the Thread class from the threading module to run the server in a separate thread
 from threading import Thread
 
+# Create a Flask app with the name 'app'
 app = Flask('')
 
+# Define a route for the root URL ('/') that returns a message indicating the app is alive
 @app.route('/')
 def home():
     return "Hello. I am alive!"
 
+# Define a function 'run' to start the Flask app
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    # Run the app on all available network interfaces (0.0.0.0) and port 8080
+    app.run(host='0.0.0.0', port=8080)
 
+# Define a function 'keep_alive' to start the web server in a separate thread
 def keep_alive():
+    # Create a new thread and provide the 'run' function as the target
     t = Thread(target=run)
+    # Start the thread to run the web server
     t.start()
